e0ad4f174f4eef15950a0b8492f4919fc3980f91
deanboole <xspiritualx@gmail.com>
2016-02-22 15:29:50 +0800

    Initial commit

---
+++ mirror.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+from colors import *
+
+c = raw_input("英文字母轉注音符號:")
+
+for letter in c:
+    if letter == "a":
+        chewing = "ㄇ"
+        print chewing ,
+    elif letter == "b":
+        chewing = "ㄖ"
+        print chewing ,
+    elif letter == "c":
+        chewing = "ㄏ"
+        print chewing ,
+    elif letter == "d":
+        chewing = "ㄎ"
+        print chewing ,
+    elif letter == "e":
+        chewing = "ㄍ"
+        print chewing ,
+    elif letter == "f":
+        chewing = "ㄑ"
+        print chewing ,
+    elif letter == "g":
+        chewing = "ㄕ"
+        print chewing ,
+    elif letter == "h":
+        chewing = "ㄘ"
+        print chewing ,
+    elif letter == "i":
+        chewing = "ㄛ"
+        print chewing ,
+    elif letter == "j":
+        chewing = "ㄨ"
+        print chewing ,
+    elif letter == "k":
+        chewing = "ㄜ"
+        print chewing ,
+    elif letter == "l":
+        chewing = "ㄠ"
+        print chewing ,
+    elif letter == "m":
+        chewing = "ㄩ"
+        print chewing ,
+    elif letter == "n":
+        chewing = "ㄙ"
+        print chewing ,
+    elif letter == "o":
+        chewing = "ㄟ"
+        print chewing ,
+    elif letter == "p":
+        chewing = "ㄣ"
+        print chewing ,
+    elif letter == "q":
+        chewing = "ㄆ"
+        print chewing ,
+    elif letter == "r":
+        chewing = "ㄐ"
+        print chewing ,
+    elif letter == "s":
+        chewing = "ㄋ"
+        print chewing ,
+    elif letter == "t":
+        chewing = "ㄔ"
+        print chewing ,
+    elif letter == "u":
+        chewing = "一"
+        print chewing ,
+    elif letter == "v":
+        chewing = "ㄒ"
+        print chewing ,
+    elif letter == "w":
+        chewing = "ㄊ"
+        print chewing ,
+    elif letter == "x":
+        chewing = "ㄌ"
+        print chewing ,
+    elif letter == "y":
+        chewing = "ㄗ"
+        print chewing ,
+    elif letter == "z":
+        chewing = "ㄈ"
+        print chewing ,
+    elif letter == "1":
+        chewing = "ㄅ"
+        print chewing ,
+    elif letter == "2":
+        chewing = "ㄉ"
+        print chewing ,
+    elif letter == "3":
+        chewing = "三聲"
+        print yellow(chewing)
+    elif letter == "4":
+        chewing = "四聲"
+        print yellow(chewing)
+    elif letter == "5":
+        chewing = "ㄓ"
+        print chewing ,
+    elif letter == "6":
+        chewing = "二聲"
+        print yellow(chewing)
+    elif letter == "7":
+        chewing = "輕聲"
+        print yellow(chewing)
+    elif letter == "8":
+        chewing = "ㄚ"
+        print chewing ,
+    elif letter == "9":
+        chewing = "ㄞ"
+        print chewing ,
+    elif letter == "0":
+        chewing = "ㄢ"
+        print chewing ,
+    elif letter == "-":
+        chewing = "ㄦ"
+        print chewing ,
+    elif letter == " ":
+        chewing = "一聲"
+        print yellow(chewing)
+    elif letter == ",":
+        chewing = "ㄝ"
+        print chewing ,
+    elif letter == ".":
+        chewing = "ㄡ"
+        print chewing ,
+    elif letter == "/":
+        chewing = "ㄥ"
+        print chewing ,
+    elif letter == "b":
+        chewing = "ㄖ"
+        print chewing ,
+
